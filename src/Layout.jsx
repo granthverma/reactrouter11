9f6a6fef5730268cc4a4f7bce235fb5005b22f83
Your Name <himanshuvermanewtonsch@gmail.com>
2024-01-12 18:24:46 +0530

    layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/Header/Header'
+import Header from './components/Header/header'
 import Footer from './components/Footer/Footer'
 import {Outlet} from 'react-router-dom'
 
